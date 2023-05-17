f40b5ccedd6f4020d2b61b23d351231f76d753e3
Long <longlp@sosene.asia>
2023-05-17 15:47:14 +0700

    Test

--- src/pages/certification_asset/real_estate/componentDetail/modals/ModalStep7Summarize.vue
+++ src/pages/certification_asset/real_estate/componentDetail/modals/ModalStep7Summarize.vue
@@ -312,7 +312,7 @@ export default {
 		},
 		test() {
 			console.log('Testing')
-			thiss.isSubmit = true
+			this.isSubmit = true
 		},
 		saveData () {
 			const round_appraise_total = this.round_appraise_total
