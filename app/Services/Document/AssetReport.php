712387a08f7fcb5c67bf41ef2f8c7213783fece9
Dat Ngo <datngo@Gigi.local>
2023-06-01 13:49:10 +0700

    test export

--- app/Services/Document/AssetReport.php
+++ app/Services/Document/AssetReport.php
@@ -94,8 +94,8 @@ class AssetReport
             $coordinates = explode(',', $object->coordinates);
             $table = $section->addTable($tableBasicStyle);
             $table->addRow(400);
-            $compare_property_doc = array_values($object->properties[0]['compare_property_doc']);
-
+            $compare_property_doc = $object->properties;
+            var_dump($object->properties[0]);
             $table->addCell(5000)->addText(CommonService::formatCompanyName($company), ['bold' => true], ['align' => JcTable::CENTER]);
             if ($object->migrate_status == ValueDefault::MIGRATION_STATUS_DEFAULT) {
                 $table->addCell(5000)->addText('Phiếu số: TSC_' . $object->id, ['italic' => true], ['align' => JcTable::END]);
