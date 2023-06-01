5c34c86f7452c04615a09b8e8490a39cb4c29aa4
Dat Ngo <datngo@Gigi.local>
2023-06-01 11:06:55 +0700

    test export

--- app/Services/Document/AssetReport.php
+++ app/Services/Document/AssetReport.php
@@ -246,11 +246,11 @@ class AssetReport
                 $cell = $table->addCell(4000);
                 $cellRun = $cell->addTextRun();
                 $cellRun->addText('- Tờ bản đồ số: ');
-                $cellRun->addText($object->doc_num, ['bold' => true]);
+                $cellRun->addText('dsađâsd',$object, ['bold' => true]);
                 $cell = $table->addCell(5000);
                 $cellRun = $cell->addTextRun();
                 $cellRun->addText('Thửa đất số: ');
-                $cellRun->addText($object->plot_num, ['bold' => true]);
+                $cellRun->addText('ddsdsdsdsđ',$object, ['bold' => true]);
 
 
                 $table = $section->addTable('coordinatesTable');
