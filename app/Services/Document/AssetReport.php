753e6977f807869a079d398cbe2d25a7523e0510
Dat Ngo <datngo@Gigi.local>
2023-06-01 11:48:21 +0700

    test export

--- app/Services/Document/AssetReport.php
+++ app/Services/Document/AssetReport.php
@@ -246,7 +246,7 @@ class AssetReport
                 $cell = $table->addCell(4000);
                 $cellRun = $cell->addTextRun();
                 $cellRun->addText('- Tờ bản đồ số: ');
-                $cellRun->addText($object->properties[0]->compare_property_doc, ['bold' => true]);
+                $cellRun->addText(json_encode($object->properties[0]->compare_property_doc[]), ['bold' => true]);
                 $cell = $table->addCell(5000);
                 $cellRun = $cell->addTextRun();
                 $cellRun->addText('Thửa đất số: ');
