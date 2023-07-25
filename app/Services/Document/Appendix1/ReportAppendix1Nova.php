e1afc7ec801f07c054b0b168ac5df51123933c32
Dat Ngo <datngo@Gigi.local>
2023-07-25 11:11:17 +0700

    fix

--- app/Services/Document/Appendix1/ReportAppendix1Nova.php
+++ app/Services/Document/Appendix1/ReportAppendix1Nova.php
@@ -313,9 +313,9 @@ class ReportAppendix1Nova extends ReportAppendix1
             $stt,
             $title,
             '-',
-            json_encode($compare1),
-            json_encode($compare2),
-            json_encode($compare3),
+            $compare1 ? number_format($compare1->asset_title, $this->countDecimals($compare1->asset_title), ',', '.')  : '-',
+            $compare2 ? number_format($compare2->asset_title, $this->countDecimals($compare2->asset_title), ',', '.')  : '-',
+            $compare3 ? number_format($compare3->asset_title, $this->countDecimals($compare3->asset_title), ',', '.')  : '-',
             false
         ];
         return $data;
