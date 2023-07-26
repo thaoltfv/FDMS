9932855b890043fe3d7675662541b7a482117242
Dat Ngo <datngo@Gigi.local>
2023-07-26 14:27:45 +0700

    fix

--- app/Services/Document/Appendix3/ReportAppendix3Donava.php
+++ app/Services/Document/Appendix3/ReportAppendix3Donava.php
@@ -25,7 +25,7 @@ class ReportAppendix3Donava extends ReportAppendix3
             'wrapDistanceRight' => 300,
         )); 
         $c1 = $table->addCell();
-        $c1->addText('PHỤ LỤC ẢNH TÀI SẢN THẨM ĐỊNH GIÁ', ['bold' => true, 'size' => 14], array('spaceAfter' => 0, 'spaceBefore' => 200));
+        $c1->addText('PHỤ LỤC ẢNH TÀI SẢN THẨM ĐỊNH GIÁ', ['bold' => true, 'size' => 14], array('spaceAfter' => 0, 'spaceBefore' => 400));
         // $section->addImage($this->logoUrl, array(
         //     'height'        => 33,
         //     'wrappingStyle' => 'inline',
@@ -37,5 +37,19 @@ class ReportAppendix3Donava extends ReportAppendix3
         $certificateDate = !empty($data->certificate_date) ? date("d/m/Y", strtotime($data->certificate_date)) : '      ';
         // $section->addText('(Kèm theo báo cáo Thẩm định giá số ' .  $certificateNum . $this->documentNumberSuffix . ', ngày ' . $certificateDate . ')', ['italic' => true, 'size' => 12], ['align' => 'center']);
         $c1->addText('(Kèm theo báo cáo Thẩm định giá số ' .  $certificateNum . $this->documentNumberSuffix . ', ngày ' . $certificateDate . ')', ['italic' => true, 'size' => 12], array('spaceAfter' => 0));
+    }
+
+    protected function printMapImage($section, $pic)
+    {
+        if (!empty($pic)) {
+            $section->addImage($pic->link, array(
+                'height' => 236,
+                'align' => 'left',
+                'space' => [
+                'line' => 1000,
+                'rule' => 'single',
+            ],));
+            $section->addText('Sơ đồ vị trí.', null, ['align' => 'center']);
+        }
     }
 }
