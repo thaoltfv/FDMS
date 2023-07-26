a0852212802657dd1ca6f77d2b9d11ec4f037acd
Dat Ngo <datngo@Gigi.local>
2023-07-26 13:35:03 +0700

    fix

--- app/Services/Document/Appendix3/ReportAppendix3Donava.php
+++ app/Services/Document/Appendix3/ReportAppendix3Donava.php
@@ -15,11 +15,12 @@ class ReportAppendix3Donava extends ReportAppendix3
     public function printTitle(Section $section, $data)
     {
         $this->setProperties($data);
-        $section->addImage($this->logoUrl, array(
+        $textrun = $section->addTextRun('pStyle');
+        $textrun->addImage($this->logoUrl, array(
             'height'        => 33,
             'wrappingStyle' => 'behind'
         ));
-        $section->addText('PHỤ LỤC ẢNH TÀI SẢN THẨM ĐỊNH GIÁ', ['bold' => true, 'size' => 14], ['align' => 'center']);
+        $textrun->addText('PHỤ LỤC ẢNH TÀI SẢN THẨM ĐỊNH GIÁ', ['bold' => true, 'size' => 14], ['align' => 'center']);
         $certificateNum = !empty($data->certificate_num) ? $data->certificate_num : '      ';
         $certificateDate = !empty($data->certificate_date) ? date("d/m/Y", strtotime($data->certificate_date)) : '      ';
         $section->addText('(Kèm theo báo cáo Thẩm định giá số ' .  $certificateNum . $this->documentNumberSuffix . ', ngày ' . $certificateDate . ')', ['italic' => true, 'size' => 12], ['align' => 'center']);
