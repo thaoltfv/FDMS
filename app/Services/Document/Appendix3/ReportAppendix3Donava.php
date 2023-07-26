33bc4b803d07a04b9225c2c83ebc011dc06da93f
Dat Ngo <datngo@Gigi.local>
2023-07-26 13:50:48 +0700

    fix

--- app/Services/Document/Appendix3/ReportAppendix3Donava.php
+++ app/Services/Document/Appendix3/ReportAppendix3Donava.php
@@ -16,13 +16,22 @@ class ReportAppendix3Donava extends ReportAppendix3
     {
         $this->setProperties($data);
         // $textrun = $section->addTextRun();
-        $section->addImage($this->logoUrl, array(
+        $table = $section->addTable();
+        $table->addRow();
+        $table->addCell(200)->addImage($this->logoUrl, array(
             'height'        => 33,
             'wrappingStyle' => 'inline',
             'wrapDistanceRight' => 30,
             'marginTop' => 1,
-        ));
-        $section->addText('PHỤ LỤC ẢNH TÀI SẢN THẨM ĐỊNH GIÁ', ['bold' => true, 'size' => 14], ['align' => 'center']);
+        )); 
+        $table->addCell()->addText('PHỤ LỤC ẢNH TÀI SẢN THẨM ĐỊNH GIÁ', ['bold' => true, 'size' => 14], ['align' => 'center']);
+        // $section->addImage($this->logoUrl, array(
+        //     'height'        => 33,
+        //     'wrappingStyle' => 'inline',
+        //     'wrapDistanceRight' => 30,
+        //     'marginTop' => 1,
+        // ));
+        // $section->addText('PHỤ LỤC ẢNH TÀI SẢN THẨM ĐỊNH GIÁ', ['bold' => true, 'size' => 14], ['align' => 'center']);
         $certificateNum = !empty($data->certificate_num) ? $data->certificate_num : '      ';
         $certificateDate = !empty($data->certificate_date) ? date("d/m/Y", strtotime($data->certificate_date)) : '      ';
         $section->addText('(Kèm theo báo cáo Thẩm định giá số ' .  $certificateNum . $this->documentNumberSuffix . ', ngày ' . $certificateDate . ')', ['italic' => true, 'size' => 12], ['align' => 'center']);
