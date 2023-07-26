869e7e9577529e8c0f1f56b46fdb8862b6f5d3a8
Dat Ngo <datngo@Gigi.local>
2023-07-26 14:15:50 +0700

    fix

--- app/Services/Document/Appendix3/ReportAppendix3Donava.php
+++ app/Services/Document/Appendix3/ReportAppendix3Donava.php
@@ -16,7 +16,7 @@ class ReportAppendix3Donava extends ReportAppendix3
     {
         $this->setProperties($data);
         // $textrun = $section->addTextRun();
-        $noSpace = array('spaceAfter' => 0);
+        // $noSpace = array('spaceAfter' => 0);
         $table = $section->addTable();
         $table->addRow();
         $table->addCell(2200)->addImage($this->logoUrl, array(
@@ -25,7 +25,7 @@ class ReportAppendix3Donava extends ReportAppendix3
             'wrapDistanceRight' => 300,
         )); 
         $c1 = $table->addCell();
-        $c1->addText('PHỤ LỤC ẢNH TÀI SẢN THẨM ĐỊNH GIÁ', ['bold' => true, 'size' => 14, 'align' => 'center'], $noSpace);
+        $c1->addText('PHỤ LỤC ẢNH TÀI SẢN THẨM ĐỊNH GIÁ', ['bold' => true, 'size' => 14], array('spaceAfter' => 0, 'spaceBefore' => 200));
         // $section->addImage($this->logoUrl, array(
         //     'height'        => 33,
         //     'wrappingStyle' => 'inline',
@@ -36,6 +36,6 @@ class ReportAppendix3Donava extends ReportAppendix3
         $certificateNum = !empty($data->certificate_num) ? $data->certificate_num : '      ';
         $certificateDate = !empty($data->certificate_date) ? date("d/m/Y", strtotime($data->certificate_date)) : '      ';
         // $section->addText('(Kèm theo báo cáo Thẩm định giá số ' .  $certificateNum . $this->documentNumberSuffix . ', ngày ' . $certificateDate . ')', ['italic' => true, 'size' => 12], ['align' => 'center']);
-        $c1->addText('(Kèm theo báo cáo Thẩm định giá số ' .  $certificateNum . $this->documentNumberSuffix . ', ngày ' . $certificateDate . ')', ['italic' => true, 'size' => 12, 'align' => 'center'], $noSpace);
+        $c1->addText('(Kèm theo báo cáo Thẩm định giá số ' .  $certificateNum . $this->documentNumberSuffix . ', ngày ' . $certificateDate . ')', ['italic' => true, 'size' => 12], array('spaceAfter' => 0));
     }
 }
