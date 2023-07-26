64fabac49d115497278ec232cb3dfee9130d18f7
Dat Ngo <datngo@Gigi.local>
2023-07-26 13:58:39 +0700

    fix

--- app/Services/Document/Appendix3/ReportAppendix3Donava.php
+++ app/Services/Document/Appendix3/ReportAppendix3Donava.php
@@ -18,7 +18,7 @@ class ReportAppendix3Donava extends ReportAppendix3
         // $textrun = $section->addTextRun();
         $table = $section->addTable();
         $table->addRow();
-        $table->addCell(3000)->addImage($this->logoUrl, array(
+        $table->addCell(2000)->addImage($this->logoUrl, array(
             'height'        => 33,
             'wrappingStyle' => 'behind',
             'wrapDistanceRight' => 300,
@@ -33,6 +33,9 @@ class ReportAppendix3Donava extends ReportAppendix3
         // $section->addText('PHỤ LỤC ẢNH TÀI SẢN THẨM ĐỊNH GIÁ', ['bold' => true, 'size' => 14], ['align' => 'center']);
         $certificateNum = !empty($data->certificate_num) ? $data->certificate_num : '      ';
         $certificateDate = !empty($data->certificate_date) ? date("d/m/Y", strtotime($data->certificate_date)) : '      ';
-        $section->addText('(Kèm theo báo cáo Thẩm định giá số ' .  $certificateNum . $this->documentNumberSuffix . ', ngày ' . $certificateDate . ')', ['italic' => true, 'size' => 12], ['align' => 'center']);
+        // $section->addText('(Kèm theo báo cáo Thẩm định giá số ' .  $certificateNum . $this->documentNumberSuffix . ', ngày ' . $certificateDate . ')', ['italic' => true, 'size' => 12], ['align' => 'center']);
+        $table->addRow();
+        $table->addCell(1800);
+        $table->addCell()->addText('(Kèm theo báo cáo Thẩm định giá số ' .  $certificateNum . $this->documentNumberSuffix . ', ngày ' . $certificateDate . ')', ['italic' => true, 'size' => 12], ['align' => 'center']);
     }
 }
