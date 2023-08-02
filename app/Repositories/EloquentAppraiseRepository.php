74b7437410fedb0a93f6c8a3dd891499dbf89edc
Dat Ngo <datngo@Gigi.local>
2023-08-02 17:06:09 +0700

    fix

--- app/Repositories/EloquentAppraiseRepository.php
+++ app/Repositories/EloquentAppraiseRepository.php
@@ -5736,7 +5736,7 @@ class  EloquentAppraiseRepository extends EloquentRepository implements Appraise
         $version = AppraiseVersionService::getVersionAppraise($appraiseId);
         $result['max_version'] = $version;
         $geo = AppraiseProperty::where('appraise_id',$appraiseId)->first();
-        $result['traffic_infomation']->geographical_location = $geo->geographical_location;
+        $result->traffic_infomation->geographical_location = $geo->geographical_location;
         $result['test'] = $geo->geographical_location;
         return $result;
     }
