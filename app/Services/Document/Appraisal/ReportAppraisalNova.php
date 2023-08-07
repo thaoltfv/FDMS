46fa97c82704c571bd984d476f2c8dd219da339f
Dat Ngo <datngo@Gigi.local>
2023-08-07 12:49:07 +0700

    fix

--- app/Services/Document/Appraisal/ReportAppraisalNova.php
+++ app/Services/Document/Appraisal/ReportAppraisalNova.php
@@ -348,9 +348,9 @@ class ReportAppraisalNova extends ReportAppraisal
     protected function step7(Section $section, $certificate)
     {
         $section->addTitle('CÁC GIẢ THIẾT VÀ GIẢ THIẾT ĐẶC BIỆT:', 1);
-        // $section->addListItem(str_replace("\n", '<w:br/>   ', $certificate->real_estate[0]->appraises->document_description), 0, null, 'bullets');
-        $section->addListItem(json_encode($certificate->real_estate), 0, null, 'bullets');
-        $section->addListItem(json_encode(json_decode($certificate)->real_estate), 0, null, 'bullets');
+        $section->addListItem(str_replace("\n", '<w:br/>   ', json_decode($certificate)->real_estate[0]->appraises->document_description), 0, null, 'bullets');
+        // $section->addListItem(json_encode($certificate->real_estate), 0, null, 'bullets');
+        // $section->addListItem(json_encode(json_decode($certificate)->real_estate), 0, null, 'bullets');
     }
     //VIII
     protected function step8(Section $section, $certificate)
