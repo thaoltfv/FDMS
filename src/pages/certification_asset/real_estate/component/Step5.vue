f1a05aabab990f178dcf35b467db2d0f4de7cd73
Dat Ngo <datngo@Gigi.local>
2023-08-08 16:34:50 +0700

    fix

--- src/pages/certification_asset/real_estate/component/Step5.vue
+++ src/pages/certification_asset/real_estate/component/Step5.vue
@@ -201,6 +201,12 @@ export default {
 			isEditLegal: false
 		}
 	},
+  mounted(){
+    console.log('--------------------',this.data)
+    if (this.data && this.data.value_base_and_approach.document_description == '') {
+      this.data.value_base_and_approach.document_description = '+ Giả thiết:\n+ Giả thiết đặc biệt:'
+    }
+  },
 	methods: {
 		handleChangeLandRemaining (event) {
 			this.$emit('changeLandRemaing', event.target.value)
--- src/pages/certification_asset/real_estate/componentDetail/Step5.vue
+++ src/pages/certification_asset/real_estate/componentDetail/Step5.vue
@@ -254,6 +254,7 @@ export default {
 		}
 	},
 	mounted () {
+    console.log('--------------------',this.data)
 	},
 	beforeUpdate () {
 	},
