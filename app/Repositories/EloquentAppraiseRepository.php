8981e04a0839afad348a3f7eed22d019f2722bf0
Dat Ngo <datngo@Gigi.local>
2023-09-19 11:08:56 +0700

    fix

--- app/Repositories/EloquentAppraiseRepository.php
+++ app/Repositories/EloquentAppraiseRepository.php
@@ -4657,7 +4657,7 @@ class  EloquentAppraiseRepository extends EloquentRepository implements Appraise
                 $appraiseValue = 'false' ;
                 // $assetValue = 0;
                 // $assetValue = $asset->properties[0]->property_detail[0]->land_type_purpose_data->acronym;
-                dd('sdsdsds', $asset);
+                dd('sdsdsds', $asset['properties']);
                 $status = false;
                 if(in_array($comparisonFactorTmp, $comparison)){
                     $status = true;
