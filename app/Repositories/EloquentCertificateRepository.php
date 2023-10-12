0ffb5e63f4c15b1d465a74da485aa3af23c53b5a
Dat Ngo <datngo@Gigi.local>
2023-10-13 00:06:23 +0700

    fix

--- app/Repositories/EloquentCertificateRepository.php
+++ app/Repositories/EloquentCertificateRepository.php
@@ -5497,7 +5497,7 @@ class  EloquentCertificateRepository extends EloquentRepository implements Certi
         $result1 = $result1->toArray();
         
         $final_result = array_merge($result, $result1);
-        // dd($result->toArray());
+        dd($final_result);
         return $final_result;
     }
 
