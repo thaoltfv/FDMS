bf0f6ed266b0ff2eb505d3ef6c9d0cb52467967a
Dat Ngo <datngo@192.168.1.6>
2023-10-12 12:06:35 +0700

    fix

--- app/Repositories/EloquentCertificateRepository.php
+++ app/Repositories/EloquentCertificateRepository.php
@@ -5219,7 +5219,7 @@ class  EloquentCertificateRepository extends EloquentRepository implements Certi
         // dd(DB::getQueryLog());
         $result = array_column($data, 'id');
         $pic = ApartmentAsset::with('pic')->where(['id' => $result])->get(['id'])->toArray();
-        dd($pic);
+        dd($result , $pic);
         foreach ($data as $item) {
             $find = array_search($item->id, array_column($pic, 'id'));
             if ($find === false)
