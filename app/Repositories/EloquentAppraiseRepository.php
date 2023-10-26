d58f8fe8388879e46751c10630d165c57a7cafb2
Dat Ngo <datngo@192.168.1.2>
2023-10-26 13:43:52 +0700

    fix

--- app/Repositories/EloquentAppraiseRepository.php
+++ app/Repositories/EloquentAppraiseRepository.php
@@ -7209,6 +7209,10 @@ class  EloquentAppraiseRepository extends EloquentRepository implements Appraise
             'tangibleAssets.buildingType:id,description'
         ];
         $result = $this->model->query()->with($with)->where('id', $id)->first($select);
+        if ($result['appraise_law'][0]['id']){
+            $clone = AppraiseLawLandDetail::quer()->where('appraise_law_id', $result['appraise_law'][0]['id'])->first();
+            dd($clone);
+        }
         return $result;
     }
 
