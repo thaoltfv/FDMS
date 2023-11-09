75883a3b7f823af5ecfe1b052180532cfabab8ee
Dat Ngo <datngo@192.168.1.7>
2023-11-09 11:54:16 +0700

    fix

--- app/Http/Controllers/Asset/CompareAssetGeneralController.php
+++ app/Http/Controllers/Asset/CompareAssetGeneralController.php
@@ -236,12 +236,16 @@ class CompareAssetGeneralController extends Controller
         try {
             $image = $request->file('image');
             $path =env('STORAGE_IMAGES') .'/'. 'comparison_assets/';
-            // $name = $path . Uuid::uuid4()->toString() . '.' . $image->getClientOriginalExtension();
-            $name = Uuid::uuid4()->toString() . '.' . $image->getClientOriginalExtension();
-            dd(Storage::disk('spaces'));
-            Storage::put($name, file_get_contents($image));
+            $name = $path . Uuid::uuid4()->toString() . '.' . $image->getClientOriginalExtension();
+            // $name = Uuid::uuid4()->toString() . '.' . $image->getClientOriginalExtension();
+            // // dd(Storage::disk('spaces'));
+            // Storage::put($name, file_get_contents($image));
             
-            $fileUrl = Storage::url($name);
+            // $fileUrl = Storage::url($name);
+
+            //test s3
+            Storage::disk('spaces')->put($name, 'public');
+            $fileUrl = Storage::disk('spaces')->url($name);
 
             // test firebase
 
