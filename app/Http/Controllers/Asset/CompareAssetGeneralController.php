9bec17fbfe9286e6fd0659e786e53ee18e8f6b4a
Dat Ngo <datngo@192.168.1.7>
2023-11-09 11:21:00 +0700

    fix

--- app/Http/Controllers/Asset/CompareAssetGeneralController.php
+++ app/Http/Controllers/Asset/CompareAssetGeneralController.php
@@ -237,7 +237,7 @@ class CompareAssetGeneralController extends Controller
             $image = $request->file('image');
             $path =env('STORAGE_IMAGES') .'/'. 'comparison_assets/';
             $name = $path . Uuid::uuid4()->toString() . '.' . $image->getClientOriginalExtension();
-            dd($name, file_get_contents($image));
+            // dd($name, file_get_contents($image));
             Storage::put($name, file_get_contents($image));
             
             $fileUrl = Storage::url($name);
