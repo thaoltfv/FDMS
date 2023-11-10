98f8c139201f14e111daf39a383641a9fe0ead45
Dat Ngo <datngo@192.168.1.11>
2023-11-10 15:59:47 +0700

    fix

--- app/Console/Commands/MigrationAllImageToS3.php
+++ app/Console/Commands/MigrationAllImageToS3.php
@@ -51,30 +51,33 @@ class MigrationAllImageToS3 extends Command
             //compareGeneralPicRepository
             $images = $this->compareGeneralPicRepository->findImage();
             Log::info("Migration compareGeneralPicRepository is start!");
-            foreach ($images as $image) {
-                $client = new Client;
-                try {
-                    $response = $client->request('GET', $image->link);
-                    $status = $response->getStatusCode();
-                   if($status == 200){
-                       try {
-                           $old_image = file_get_contents( $image->link);
-                           $path =env('STORAGE_IMAGES') .'/'. 'comparison_assets/';
-                           $last_part = substr(strrchr($image->link, "."), 1);
-                        //    dd($last_part);
-                           $name = $path . Uuid::uuid4()->toString() . '.' .$last_part;
-                           Storage::put($name, $old_image);
-                           $fileUrl = Storage::url($name);
-                           $this->compareGeneralPicRepository->updateGeneralPic($image->id,['link'=>$fileUrl]);
-                       } catch (\Exception $e) {
-                           Log::error('Migration compareGeneralPicRepository is error with message  ' . $e);
-                       }
-                   }
-                } catch (\Exception $e) {
-                    $this->compareGeneralPicRepository->deleteGeneralPic($image->id);
-                    Log::error('Migration compareGeneralPicRepository is error with message  ' . $e);
+            if ($images) {
+                foreach ($images as $image) {
+                    $client = new Client;
+                    try {
+                        $response = $client->request('GET', $image->link);
+                        $status = $response->getStatusCode();
+                    if($status == 200){
+                        try {
+                            $old_image = file_get_contents( $image->link);
+                            $path =env('STORAGE_IMAGES') .'/'. 'comparison_assets/';
+                            $last_part = substr(strrchr($image->link, "."), 1);
+                            //    dd($last_part);
+                            $name = $path . Uuid::uuid4()->toString() . '.' .$last_part;
+                            Storage::put($name, $old_image);
+                            $fileUrl = Storage::url($name);
+                            $this->compareGeneralPicRepository->updateGeneralPic($image->id,['link'=>$fileUrl]);
+                        } catch (\Exception $e) {
+                            Log::error('Migration compareGeneralPicRepository is error with message  ' . $e);
+                        }
+                    }
+                    } catch (\Exception $e) {
+                        $this->compareGeneralPicRepository->deleteGeneralPic($image->id);
+                        Log::error('Migration compareGeneralPicRepository is error with message  ' . $e);
+                    }
                 }
             }
+            Log::info("Migration compareGeneralPicRepository is end!");
             //appraise_pics
             $query = 'link not ilike ' . "'%fv-trial.s3-ap-southeast-1.amazonaws.com%'";
             $images = AppraisePic::query()->select()
@@ -83,39 +86,42 @@ class MigrationAllImageToS3 extends Command
                     ->whereNotNull('link')
                     ->get();
             Log::info("Migration appraise_pics is start!");
-            foreach ($images as $image) {
-                $client = new Client;
-                try {
-                    $response = $client->request('GET', $image->link);
-                    $status = $response->getStatusCode();
-                   if($status == 200){
-                       try {
-                           $old_image = file_get_contents( $image->link);
-                           $check_dic_certi = substr($image->link, strpos($image->link,"certification_assets"), 20);
-                           $check_dic_compa = substr($image->link, strpos($image->link,"comparison_assets"), 17);
-                           if ($check_dic_certi == 'certification_assets') {
-                            $path =env('STORAGE_IMAGES') .'/'. 'certification_assets/';
-                           } else if ($check_dic_compa == 'comparison_assets') {
-                            $path =env('STORAGE_IMAGES') .'/'. 'comparison_assets/';
-                           } else {
-                            $path =env('STORAGE_IMAGES') .'/'. 'other/';
-                           }
-                           
-                           $last_part = substr(strrchr($image->link, "."), 1);
-                        //    dd($last_part);
-                           $name = $path . Uuid::uuid4()->toString() . '.' .$last_part;
-                           Storage::put($name, $old_image);
-                           $fileUrl = Storage::url($name);
-                           AppraisePic::query()->where('id', $image->id)->update(['link' => $fileUrl]);;
-                       } catch (\Exception $e) {
-                           Log::error('Migration appraise_pics is error with message  ' . $e);
-                       }
-                   }
-                } catch (\Exception $e) {
-                    // $this->compareGeneralPicRepository->deleteGeneralPic($image->id);
-                    Log::error('Migration appraise_pics is error with message  ' . $e);
+            if ($images) {
+                foreach ($images as $image) {
+                    $client = new Client;
+                    try {
+                        $response = $client->request('GET', $image->link);
+                        $status = $response->getStatusCode();
+                    if($status == 200){
+                        try {
+                            $old_image = file_get_contents( $image->link);
+                            $check_dic_certi = substr($image->link, strpos($image->link,"certification_assets"), 20);
+                            $check_dic_compa = substr($image->link, strpos($image->link,"comparison_assets"), 17);
+                            if ($check_dic_certi == 'certification_assets') {
+                                $path =env('STORAGE_IMAGES') .'/'. 'certification_assets/';
+                            } else if ($check_dic_compa == 'comparison_assets') {
+                                $path =env('STORAGE_IMAGES') .'/'. 'comparison_assets/';
+                            } else {
+                                $path =env('STORAGE_IMAGES') .'/'. 'other/';
+                            }
+                            
+                            $last_part = substr(strrchr($image->link, "."), 1);
+                            //    dd($last_part);
+                            $name = $path . Uuid::uuid4()->toString() . '.' .$last_part;
+                            Storage::put($name, $old_image);
+                            $fileUrl = Storage::url($name);
+                            AppraisePic::query()->where('id', $image->id)->update(['link' => $fileUrl]);;
+                        } catch (\Exception $e) {
+                            Log::error('Migration appraise_pics is error with message  ' . $e);
+                        }
+                    }
+                    } catch (\Exception $e) {
+                        // $this->compareGeneralPicRepository->deleteGeneralPic($image->id);
+                        Log::error('Migration appraise_pics is error with message  ' . $e);
+                    }
                 }
             }
+            Log::info("Migration appraise_pics is end!");
 
         Log::info('Migration images is end!');
     }
