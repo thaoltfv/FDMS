e0a5be2a0cd12f4f45ad5a43426626c91eb477c2
khanh <khanhcao0502@gmail.com>
2023-11-29 16:21:43 +0700

    Update .eslintignore to exclude /src/assets/ directory

---
+++ src/models/PreCertificate.js
@@ -0,0 +1,73 @@
+import { isEmpty } from "lodash-es";
+import Model from "./Model.js";
+
+export default class Certificate extends Model {
+	buildUrl(request) {
+		const { params } = request;
+		return ["pre-certificate", ...params];
+	}
+	static async getAppraiseLaws() {
+		let resp = localStorage.getItem("appraise-laws");
+		if (isEmpty(resp)) {
+			resp = await new this().request({
+				method: "GET",
+				url: `appraise-laws`,
+				isStatic: true
+			});
+			localStorage.setItem("appraise-laws", JSON.stringify(resp));
+		} else resp = JSON.parse(resp);
+		return resp;
+	}
+	static async getAppraisers() {
+		// let resp = localStorage.getItem('appraisers')
+		// if (isEmpty(resp)) {
+		let resp = await new this().request({
+			method: "GET",
+			url: `appraisers`,
+			isStatic: true
+		});
+		localStorage.setItem("appraisers", JSON.stringify(resp));
+		// } else resp = JSON.parse(resp)
+		return resp;
+	}
+	static async getAppraisersManager() {
+		// return (new this()).request({ method: 'GET', url: `appraisers?search=150`, isStatic: true })
+		// return (new this()).request({ method: 'GET', url: `appraisers?search=TONG-GIAM-DOC`, isStatic: true })
+		return new this().request({
+			method: "GET",
+			url: `appraisers?is_legal_representative=1`,
+			isStatic: true
+		});
+	}
+	static async getAppraiseOthers() {
+		let resp = localStorage.getItem("appraise-others");
+		if (isEmpty(resp)) {
+			resp = await new this().request({
+				method: "GET",
+				url: `appraise-others`,
+				isStatic: true
+			});
+			localStorage.setItem("appraise-others", JSON.stringify(resp));
+		} else resp = JSON.parse(resp);
+		return resp;
+	}
+	static async getAppraiseConstructions() {
+		let resp = localStorage.getItem("appraisal-constructions");
+		if (isEmpty(resp)) {
+			resp = await new this().request({
+				method: "GET",
+				url: `appraisal-constructions`,
+				isStatic: true
+			});
+			localStorage.setItem("appraisal-constructions", JSON.stringify(resp));
+		} else resp = JSON.parse(resp);
+		return resp;
+	}
+	static async getCertificate(id) {
+		return new this().request({
+			method: "GET",
+			url: `pre-certificate/${id}`,
+			isStatic: true
+		});
+	}
+}
