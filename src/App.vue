33e68e592b04d9678bf9fc7d284bf595dda79cb6
Dat Ngo <datngo@192.168.1.8>
2023-12-05 09:27:33 +0700

    fix

--- src/App.vue
+++ src/App.vue
@@ -28,8 +28,8 @@ export default {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P5Q3F6R6');<\/script>`)
-		$('body').append(`<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P5Q3F6R6"
+})(window,document,'script','dataLayer','GTM-N332BZF2');<\/script>`)
+		$('body').append(`<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N332BZF2"
 height="0" width="0" style="display:none;visibility:hidden"><\/iframe><\/noscript>`)
 	}
 }
