88dde4fbe4740bcae2f72f7eef246d8c3d28d513
Dat Ngo <datngo@192.168.1.10>
2023-12-19 16:46:27 +0700

    fix formula clcl

--- app/Repositories/EloquentPreCertificateRepository.php
+++ app/Repositories/EloquentPreCertificateRepository.php
@@ -1220,11 +1220,11 @@ class  EloquentPreCertificateRepository extends EloquentRepository implements Pr
         }
 
         $result = $result->orderByDesc('pre_certificates.updated_at');
-
+        dd($result);
         $result = $result
             ->forPage($page, $perPage)
             ->paginate($perPage);
-        dd($result);
+        
         // foreach ($result as $stt => $item) {
         //     $result[$stt]->append('detail_list_id');
         //     // $result[$stt]->append('certificate_asset_price');
