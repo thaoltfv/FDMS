59c673aed87db4298ef014e99d947b9ce4982744
Dat Ngo <datngo@192.168.1.10>
2023-12-19 16:55:25 +0700

    fix formula clcl

--- app/Repositories/EloquentPreCertificateRepository.php
+++ app/Repositories/EloquentPreCertificateRepository.php
@@ -1152,6 +1152,7 @@ class  EloquentPreCertificateRepository extends EloquentRepository implements Pr
         $result = QueryBuilder::for($this->model)
             ->with($with)
             ->select($select);
+        dd($result->toSql());
         dd($result->forPage($page, $perPage)->paginate($perPage));
         //// command tạm - sẽ xử lý phân quyền sau
         $role = $user->roles->last();
