f60f0268992e7562e9eab1af241438417b346727
Dat Ngo <datngo@Gigi.local>
2023-12-20 17:00:20 +0700

    fix formula clcl

--- app/Services/Document/Appendix2/ReportAppendix2Nova.php
+++ app/Services/Document/Appendix2/ReportAppendix2Nova.php
@@ -37,8 +37,9 @@ class ReportAppendix2Nova extends ReportAppendix2
             $province_id = $this->realEstates->appraises->province_id;
             $province_name = Province::query()->where('id', $province_id)->first()->name;
             $law_province = AppraiseLawDocument::query()->where(['type' => 'XAY_DUNG', 'provinces' => $province_name])->get()->toArray();
-            dd($law_province);
+           
             foreach ($law_province as $law) {
+                dd($law);
                 if ($law->provinces === $province_name) {
                     array_push($local_law,$law);
                 }
