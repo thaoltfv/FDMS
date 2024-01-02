568b09b57d32fa055b1c7cbf98377842802465dc
Dat Ngo <datngo@192.168.1.4>
2024-01-02 11:48:23 +0700

    test doc

--- app/Repositories/EloquentCertificateRepository.php
+++ app/Repositories/EloquentCertificateRepository.php
@@ -289,12 +289,13 @@ class  EloquentCertificateRepository extends EloquentRepository implements Certi
 
                 // Lưu dữ liệu binary vào một tệp tạm thời
                 $tempFilePath = storage_path('app/public/temp.docx');
+                dd($tempFilePath);
                 file_put_contents($tempFilePath, base64_decode($file));
                 // Sử dụng thư viện để đọc file .doc
                 $phpWord = new PhpWord();
                 $phpWord = IOFactory::load($tempFilePath);
 
-                dd($tempFilePath, $phpWord);
+                // dd($tempFilePath, $phpWord);
 
                 // Xử lý nội dung theo nhu cầu của bạn
                 // ...
