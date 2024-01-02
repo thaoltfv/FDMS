9342318d590190ba2d57634b0c669a5d0da1b9b6
Dat Ngo <datngo@192.168.1.4>
2024-01-02 11:12:03 +0700

    test doc

--- app/Repositories/EloquentCertificateRepository.php
+++ app/Repositories/EloquentCertificateRepository.php
@@ -289,9 +289,13 @@ class  EloquentCertificateRepository extends EloquentRepository implements Certi
 
                 if (isset($files) && !empty($files)) {
                     foreach ($files as $file) {
+
+                        // Lưu dữ liệu binary vào một tệp tạm thời
+                        $tempFilePath = storage_path('app/temp.docx');
+                        file_put_contents($tempFilePath, base64_decode($file));
                         // Sử dụng thư viện để đọc file .doc
                         $phpWord = new PhpWord();
-                        $phpWord->load($file);
+                        $phpWord->load($tempFilePath);
 
                         // Lấy nội dung từ template
                         $sections = $phpWord->getSections();
