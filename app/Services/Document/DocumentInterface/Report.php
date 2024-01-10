4ee56d16064e12f5c080567eb71e262cc9050595
Dat Ngo <datngo@Gigi.local>
2024-01-10 13:54:21 +0700

    change header

--- app/Services/Document/DocumentInterface/Report.php
+++ app/Services/Document/DocumentInterface/Report.php
@@ -67,6 +67,14 @@ class Report implements ReportInterface
         ],
     ];
 
+    protected $styleImageHeader1 = [
+        'align' => 'center',
+        'height' => 33,
+        'space' => [
+            'line' => 1000,
+        ],
+    ];
+
     protected $styleImageFooter = [
         'width' => 488,
         'align' => 'left',
