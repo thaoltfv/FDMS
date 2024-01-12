1530d6c319216be2fc5cfdae33a0edd06577aa69
Dat Ngo <datngo@192.168.1.8>
2024-01-12 16:05:51 +0700

    test change png

--- app/Http/Controllers/Asset/CompareAssetGeneralController.php
+++ app/Http/Controllers/Asset/CompareAssetGeneralController.php
@@ -263,7 +263,7 @@ class CompareAssetGeneralController extends Controller
                     // Xử lý lỗi khi chuyển đổi
                     dd($image->getError());
                 } else {
-                    dd(realpath(public_path('storage/'.$jpgPath)));
+                    dd($jpgPath);
                     // Upload tệp JPG lên S3
                     $s3Path = $path . Uuid::uuid4()->toString() . '.jpg';
                     Storage::put($s3Path, Storage::disk('public')->get(realpath(public_path('storage/'.$jpgPath))));
