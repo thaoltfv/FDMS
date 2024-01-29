b1afaef062f01664d54fa809cb35e1adb940955c
khanh <khanhcao0502@gmail.com>
2024-01-29 14:42:06 +0700

    Refactor email sending logic and add email template

--- app/Mail/JustTesting.php
+++ app/Mail/JustTesting.php
@@ -11,13 +11,27 @@ class JustTesting extends Mailable
 {
     use Queueable, SerializesModels;
 
+    protected $emailReceive;
+    public $subject;
+    protected $markdown;
+    protected $name;
+    protected $message;
+    protected $link;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($emailReceive, $subject, $markdown, $name, $message, $link)
     {
+        $this->emailReceive = $emailReceive;
+        $this->subject = $subject;
+        $this->markdown = $markdown;
+        $this->name = $name;
+        $this->message = $message;
+        $this->link = $link;
+
         $this->onQueue('notifications');
     }
 
@@ -28,13 +42,13 @@ class JustTesting extends Mailable
      */
     public function build()
     {
-        return $this->to('longlp@sosene.asia')
-                   ->subject('[HSTD - 218] Chuyển sang trạng thái thẩm định')
-                   ->markdown('emails.notifications.update')
+        return $this->to($this->emailReceive)
+                   ->subject($this->subject)
+                   ->markdown($this->markdown)
                    ->with([
-                     'name' => 'Lê Phi Long',
-                     'message' => 'HSTD_218 đã được Lê Phi Long chuyển sang trạng thái Thẩm định',
-                     'link' => '#'
+                     'name' => $this->name,
+                     'message' => $this->message,
+                     'link' => $this->link
                    ]);
     }
 }
