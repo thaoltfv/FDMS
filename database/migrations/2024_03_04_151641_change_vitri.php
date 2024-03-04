4aa0d45f5e60d5c5cfdf09a7334798f689c4b8a4
Dat Ngo <datngo@192.168.1.6>
2024-03-04 18:13:06 +0700

    change field

---
+++ database/migrations/2024_03_04_151641_change_vitri.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeVitri extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasColumn('appraise_comparison_factor', 'appraise_title')) {
+            Schema::table('appraise_comparison_factor', function (Blueprint $table) {
+                $table->text('appraise_title')->change();
+            });
+        }
+        if (Schema::hasColumn('appraise_comparison_factor', 'asset_title')) {
+            Schema::table('appraise_comparison_factor', function (Blueprint $table) {
+                $table->text('asset_title')->change();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // if (!Schema::hasColumn('appraise_comparison_factor', 'is_synced_els')) {
+        //     Schema::table('appraise_comparison_factor', function (Blueprint $table) {
+        //         $table->dropColumn('is_synced_els');
+        //     });
+        // }
+    }
+}
