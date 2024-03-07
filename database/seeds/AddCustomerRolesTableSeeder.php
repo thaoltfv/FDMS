4fbb6e2a0abcd8dae1ace34bfd044c2ba19f8466
khanh <khanhcao0502@gmail.com>
2024-03-08 03:11:32 +0700

    Refactor role and permission assignment in AddCustomerRolesTableSeeder

--- database/seeds/AddCustomerRolesTableSeeder.php
+++ database/seeds/AddCustomerRolesTableSeeder.php
@@ -23,13 +23,8 @@ class AddCustomerRolesTableSeeder extends Seeder
             );
         }
         $guestRole = Role::query()->where(['name' => RoleDefault::USER['role'], 'guard_name' => 'api', 'role_name' => RoleDefault::USER['name_2']])->first();
-        Log::info('guestRole: ', ['result' => $guestRole->toArray()]);
         foreach (PermissionsDefault::PERMISSIONS as $permission) {
             $screen = ScreensDefault::ACCOUNTING_SCREEN;
-
-            $permissionResult = Permission::where('name', '=', $permission . '_' . $screen)->get();
-
-            Log::info('Permission Result: ', ['result' => $permissionResult->toArray()]);
             $guestRole->givePermissionTo(Permission::where('name', '=', $permission . '_' . $screen)->get());
         }
         $adminRole = Role::query()->where(['name' => RoleDefault::ADMIN['role'], 'guard_name' => 'api', 'role_name' => RoleDefault::ADMIN['name_2']])->first();
@@ -42,15 +37,15 @@ class AddCustomerRolesTableSeeder extends Seeder
             $screen = ScreensDefault::ACCOUNTING_SCREEN;
             $rootAdminRole->givePermissionTo(Permission::where('name', '=', $permission . '_' . $screen)->get());
         }
-        // $superAdminRole = Role::query()->where(['name' => RoleDefault::SUPER_ADMIN['role'], 'guard_name' => 'api', 'role_name' => RoleDefault::SUPER_ADMIN['name_2']])->first();
-        // foreach (PermissionsDefault::PERMISSIONS as $permission) {
-        //     $screen = ScreensDefault::PAYMENT_SCREEN;
-        //     $superAdminRole->givePermissionTo(Permission::where('name', '=', $permission . '_' . $screen)->get());
-        // }
-        // $subAdminRole = Role::query()->where(['name' => RoleDefault::SUB_ADMIN['role'], 'guard_name' => 'api', 'role_name' => RoleDefault::SUB_ADMIN['name_2']])->first();
-        // foreach (PermissionsDefault::PERMISSIONS as $permission) {
-        //     $screen = ScreensDefault::PAYMENT_SCREEN;
-        //     $subAdminRole->givePermissionTo(Permission::where('name', '=', $permission . '_' . $screen)->get());
-        // }
+        $superAdminRole = Role::query()->where(['name' => RoleDefault::SUPER_ADMIN['role'], 'guard_name' => 'api', 'role_name' => RoleDefault::SUPER_ADMIN['name_2']])->first();
+        foreach (PermissionsDefault::PERMISSIONS as $permission) {
+            $screen = ScreensDefault::ACCOUNTING_SCREEN;
+            $superAdminRole->givePermissionTo(Permission::where('name', '=', $permission . '_' . $screen)->get());
+        }
+        $subAdminRole = Role::query()->where(['name' => RoleDefault::SUB_ADMIN['role'], 'guard_name' => 'api', 'role_name' => RoleDefault::SUB_ADMIN['name_2']])->first();
+        foreach (PermissionsDefault::PERMISSIONS as $permission) {
+            $screen = ScreensDefault::ACCOUNTING_SCREEN;
+            $subAdminRole->givePermissionTo(Permission::where('name', '=', $permission . '_' . $screen)->get());
+        }
     }
 }
