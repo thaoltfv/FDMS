0c4e1212b90b204960f577ba821eabf30e7c9334
khanh <khanhcao0502@gmail.com>
2024-03-08 02:53:10 +0700

    Update payment screen to accounting screen

--- database/seeds/AddCustomerRolesTableSeeder.php
+++ database/seeds/AddCustomerRolesTableSeeder.php
@@ -17,22 +17,22 @@ class AddCustomerRolesTableSeeder extends Seeder
     public function run()
     {
         foreach (PermissionsDefault::PERMISSIONS as $permission) {
-            $screen = ScreensDefault::PAYMENT_SCREEN;
+            $screen = ScreensDefault::ACCOUNTING_SCREEN;
             Permission::create(['name' => $permission . '_' . $screen, 'guard_name' => 'api']);
         }
         $guestRole = Role::query()->where(['name' => RoleDefault::USER['role'], 'guard_name' => 'api', 'role_name' => RoleDefault::USER['name']])->first();
         foreach (PermissionsDefault::PERMISSIONS as $permission) {
-            $screen = ScreensDefault::PAYMENT_SCREEN;
+            $screen = ScreensDefault::ACCOUNTING_SCREEN;
             $guestRole->givePermissionTo(Permission::where('name', '=', $permission . '_' . $screen)->get());
         }
         $adminRole = Role::query()->where(['name' => RoleDefault::ADMIN['role'], 'guard_name' => 'api', 'role_name' => RoleDefault::ADMIN['name']])->first();
         foreach (PermissionsDefault::PERMISSIONS as $permission) {
-            $screen = ScreensDefault::PAYMENT_SCREEN;
+            $screen = ScreensDefault::ACCOUNTING_SCREEN;
             $adminRole->givePermissionTo(Permission::where('name', '=', $permission . '_' . $screen)->get());
         }
         $rootAdminRole = Role::query()->where(['name' => RoleDefault::ROOT_ADMIN['role'], 'guard_name' => 'api', 'role_name' => RoleDefault::ROOT_ADMIN['name']])->first();
         foreach (PermissionsDefault::PERMISSIONS as $permission) {
-            $screen = ScreensDefault::PAYMENT_SCREEN;
+            $screen = ScreensDefault::ACCOUNTING_SCREEN;
             $rootAdminRole->givePermissionTo(Permission::where('name', '=', $permission . '_' . $screen)->get());
         }
         // $superAdminRole = Role::query()->where(['name' => RoleDefault::SUPER_ADMIN['role'], 'guard_name' => 'api', 'role_name' => RoleDefault::SUPER_ADMIN['name']])->first();
