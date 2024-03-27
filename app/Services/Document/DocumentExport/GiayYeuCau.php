39cdf3e36131c54bcb0faf88daf4cc7a24d3df90
Hoang11012001 <lehuyhoang05022017@gmail.com>
2024-03-27 10:43:43 +0700

    check

--- app/Services/Document/DocumentExport/GiayYeuCau.php
+++ app/Services/Document/DocumentExport/GiayYeuCau.php
@@ -236,9 +236,10 @@ class GiayYeuCau
         $section->addListItem("Nguồn gốc tài sản (Nhà nước/ không phải thuộc Nhà nước): Không phải thuộc Nhà nước", 0, [], 'bullets', $indentFistLine);
         $section->addListItem("Ngày giờ, địa điểm khảo sát: 04/03/2024 tại Căn hộ số 10.32, Chung cư Flora Anh Đào (Ehome 6), 619 Đỗ Xuân Hợp, phường Phước Long B, Quận 9, TP.HCM.", 0, [], 'bullets', $indentFistLine);
         $section->addListItem("Tên, điện thoại người liên hệ:                           ; Điện thoại:                   ", 0, [], 'bullets', $indentFistLine);
-        $listItemRun  = $section->addListItemRun('bullets');
+        $listItemRun  = $section->addListItemRun('Heading2');
         $listItemRun->addText("Các hồ sơ, dữ liệu cá nhân, cung cấp gồm: ");
         $listItemRun->addText("01 Bản Giấy chứng nhận quyền sử dụng đất quyền sở hữu nhà ở và tài sản khác gắn liền với đất số CK 096662 số vào sổ cấp GCN:CS23305/DA ngày 30/05/2018 do Sở Tài Nguyên và Môi Trường thành phố Hồ Chí Minh cấp.", ['italic' => true]);
+
         $section->addListItem("Số bản chứng thư yêu cầu cấp: 02 bản chính bằng tiếng Việt.", 0, [], 'bullets', $indentFistLine);
         $section->addText("   Tôi đồng ý cung cấp các Hồ sơ, Dữ liệu cá nhân như trên cho Công ty TNHH Thẩm định giá Nova, Công ty Nova được phép xử lý các dữ liệu được cung cấp để công ty tiến hành thu thập thông tin, lập hồ sơ Thẩm định giá tài sản phù hợp với mục đích được yêu cầu tại văn bản này.", []);
         $section->addText("   Tôi cam kết thanh toán đủ phí dịch vụ theo quy định Công ty TNHH thẩm định giá Nova.", []);
