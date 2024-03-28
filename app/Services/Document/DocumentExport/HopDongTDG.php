25bbd69f0d833898419bb0bf60d848684b871122
khanh <khanhcao0502@gmail.com>
2024-03-28 18:52:56 +0700

    Refactor document export code to improve readability and formatting

--- app/Services/Document/DocumentExport/HopDongTDG.php
+++ app/Services/Document/DocumentExport/HopDongTDG.php
@@ -196,26 +196,60 @@ class HopDongTDG
         ]);
         $alignBoth = ['align' => 'both'];
         $row1 = $table->addRow(100, array('tblHeader' => false, 'cantSplit' => false));
-        $row1->addCell(1900, $cellVCentered)->addText('BÊN A', ['bold' => true,],  $alignBoth);
-        $row1->addCell(8000, $cellVCentered)->addText(': ' . $certificate->petitioner_name, ['bold' => true],  $alignBoth);
+        $row1->addCell(1800, $cellVCentered)->addText('BÊN A', ['bold' => true,],  $alignBoth);
+        $row1->addCell(8100, $cellVCentered)->addText(': ' . $certificate->petitioner_name, ['bold' => true],  $alignBoth);
 
         $row2 = $table->addRow(100, array('tblHeader' => false, 'cantSplit'
         => false));
-        $row2->addCell(1900, $cellVCentered)->addText('-    Địa chỉ', null,  $alignBoth);
-        $row2->addCell(8000, $cellVCentered)->addText(': ' . $certificate->petitioner_address, null,  $alignBoth);
+        $row2->addCell(1800, $cellVCentered)->addText('-    Địa chỉ', null,  $alignBoth);
+        $row2->addCell(8100, $cellVCentered)->addText(': ' . $certificate->petitioner_address, null,  $alignBoth);
 
         $row3 = $table->addRow(100, array(
             'tblHeader' => false,
             'cantSplit' => false
         ));
-        $row3->addCell(1900, $cellVCentered)->addText('-    Số CCCD', null,  $alignBoth);
-        $row3->addCell(8000, $cellVCentered)->addText(': ' . $certificate->petitioner_identity_card, null,  $alignBoth);
+        $row3->addCell(1800, $cellVCentered)->addText('-    Số CCCD', null,  $alignBoth);
+        $row3->addCell(8100, $cellVCentered)->addText(': ' . $certificate->petitioner_identity_card, null,  $alignBoth);
 
         $row4 = $table->addRow(100, array('tblHeader' => false, 'cantSplit' => false));
-        $row4->addCell(1900, $cellVCentered)->addText('-    Số điện thoại', null,  $alignBoth);
-        $row4->addCell(8000, $cellVCentered)->addText(': ' . $certificate->petitioner_phone, null,  $alignBoth);
+        $row4->addCell(1800, $cellVCentered)->addText('-    Số điện thoại', null,  $alignBoth);
+        $row4->addCell(8100, $cellVCentered)->addText(': ' . $certificate->petitioner_phone, null,  $alignBoth);
 
 
+        $row5 = $table->addRow(100, array('tblHeader' => false, 'cantSplit' => false));
+        $row5->addCell(1800, $cellVCentered)->addText('BÊN B', ['bold' => true,],  $alignBoth);
+        $row5->addCell(8100, $cellVCentered)->addText(': CÔNG TY TNHH THẨM ĐỊNH GIÁ NOVA', ['bold' => true],  $alignBoth);
+
+        $row6 = $table->addRow(100, array('tblHeader' => false, 'cantSplit'
+        => false));
+        $row6->addCell(1800, $cellVCentered)->addText('-    Địa chỉ', null,  $alignBoth);
+        $row6->addCell(8100, $cellVCentered)->addText(': Số 728-730 Võ Văn Kiệt, Phường 1, Quận 5, TP.HCM', null,  $alignBoth);
+
+        $row7 = $table->addRow(100, array(
+            'tblHeader' => false,
+            'cantSplit' => false
+        ));
+        $row7->addCell(1800, $cellVCentered)->addText('-    Điện thoại bàn', null,  $alignBoth);
+        $row7->addCell(8100, $cellVCentered)->addText(': (028) 3920 6779 – Fax: (028) 3920 6778', null,  $alignBoth);
+
+        $row8 = $table->addRow(100, array('tblHeader' => false, 'cantSplit' => false));
+        $row8->addCell(1800, $cellVCentered)->addText('-   Mã số thuế', null,  $alignBoth);
+        $row8->addCell(8100, $cellVCentered)->addText(': 0314514140', null,  $alignBoth);
+
+
+        $row9 = $table->addRow(100, array('tblHeader' => false, 'cantSplit' => false));
+        $row9->addCell(1800, $cellVCentered)->addText('-   Tài khoản số', null,  $alignBoth);
+        $row9->addCell(8100, $cellVCentered)->addText(': 3101 00024 27729 tại Ngân hàng TMCP Đầu tư và Phát triển Việt Nam – CN Hồ Chí Minh – PGD Trần Hưng Đạo', null,  $alignBoth);
+
+        $row9 = $table->addRow(100, array('tblHeader' => false, 'cantSplit' => false));
+        $row9->addCell(1800, $cellVCentered)->addText('-   Đại diện', null,  $alignBoth);
+        $row9->addCell(8100, $cellVCentered)->addText(': ' . 'Ông ' . $certificate->appraiser_manager->name, null,  $alignBoth);
+
+        $section->addText(
+            "Sau khi thương lượng, hai bên đồng ý ký kết hợp đồng cung cấp dịch vụ thẩm định giá tài sản với các điều kiện và điều khoản như sau",
+            null,
+            ['align' => 'both', 'indentation' => ['firstLine' => \PhpOffice\PhpWord\Shared\Converter::inchToTwip(0.23)]]
+        );
 
 
         $footer = $section->addFooter();
