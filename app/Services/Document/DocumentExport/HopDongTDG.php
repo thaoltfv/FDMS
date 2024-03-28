09712f00744a1256c57e7a52f1a608334d209342
khanh <khanhcao0502@gmail.com>
2024-03-29 00:31:54 +0700

    Add border to table in HopDongTDG.php

--- app/Services/Document/DocumentExport/HopDongTDG.php
+++ app/Services/Document/DocumentExport/HopDongTDG.php
@@ -277,6 +277,7 @@ class HopDongTDG
         $row1->addCell(8700, $cellVTop)->addText('Tài sản thẩm định giá : Quyền sở hữu căn hộ  (Theo Giấy chứng nhận quyền sử dụng đất quyền sở hữu nhà ở và tài sản khác gắn liền với đất số CK 096662 số vào sổ cấp GCN:CS23305/DA ngày 30/05/2018 do Sở Tài Nguyên và Môi Trường thành phố Hồ Chí Minh cấp).', null,  $alignBoth);
 
         $table = $section->addTable([
+            'borderSize' => 1,
             'align' => JcTable::START,
             'width' => 100 * 50,
             'unit' => 'pct'
