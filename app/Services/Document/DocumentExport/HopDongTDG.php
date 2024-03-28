143da8365daed99cd93efd05f2af3f0741d51bcb
khanh <khanhcao0502@gmail.com>
2024-03-28 17:59:48 +0700

    Fix margin left in HopDongTDG.php

--- app/Services/Document/DocumentExport/HopDongTDG.php
+++ app/Services/Document/DocumentExport/HopDongTDG.php
@@ -128,7 +128,7 @@ class HopDongTDG
             'marginTop' => Converter::inchToTwip(0.75),
             // 'marginBottom' => Converter::inchToTwip(.6),
             'marginRight' => Converter::inchToTwip(0.3),
-            'marginLeft' => Converter::inchToTwip(0.3)
+            'marginLeft' => Converter::inchToTwip(0.6)
         ];
 
         $check = "";
@@ -140,16 +140,16 @@ class HopDongTDG
             'unit' => 'pct'
         ]);
         $row1 = $table->addRow(100, array('tblHeader' => false, 'cantSplit' => false));
-        $row1->addCell(4000, $cellVCentered)->addText('CÔNG TY TNHH', ['bold' => true,], $cellHCentered);
-        $row1->addCell(500, $cellVCentered)->addText('', ['bold' => true,], $cellHCentered);
+        $row1->addCell(3500, $cellVCentered)->addText('CÔNG TY TNHH', ['bold' => true,], $cellHCentered);
+        $row1->addCell(1000, $cellVCentered)->addText('', ['bold' => true,], $cellHCentered);
         $row1->addCell(5700, $cellVCentered)->addText('CỘNG HÒA XÃ HỘI CHỦ NGHĨA VIỆT NAM', ['bold' => true], $cellHCentered);
         $row2 = $table->addRow(400, array('tblHeader' => false, 'cantSplit' => false));
-        $row2->addCell(4000, $cellVCentered)->addText('THẨM ĐỊNH GIÁ NOVA', ['bold' => true,  'underline' => 'single'], $cellHCentered);
-        $row2->addCell(200, $cellVCentered)->addText('', ['bold' => true,], $cellHCentered);
+        $row2->addCell(3500, $cellVCentered)->addText('THẨM ĐỊNH GIÁ NOVA', ['bold' => true,  'underline' => 'single'], $cellHCentered);
+        $row2->addCell(1000, $cellVCentered)->addText('', ['bold' => true,], $cellHCentered);
         $row2->addCell(5700, $cellVCentered)->addText('Độc lập – Tự do - Hạnh phúc', ['bold' => true,   'underline' => 'single'], $cellHCentered);
         $row3 = $table->addRow(400, array('tblHeader' => false, 'cantSplit' => false));
-        $row3->addCell(4000, $cellVCentered)->addText('Số: 24145/HĐTĐG-HCM ', null, $cellHCentered);
-        $row3->addCell(500, $cellVCentered)->addText(
+        $row3->addCell(3500, $cellVCentered)->addText('Số: 24145/HĐTĐG-HCM ', null, $cellHCentered);
+        $row3->addCell(1000, $cellVCentered)->addText(
             '',
             ['bold' => true,],
             $cellHCentered
