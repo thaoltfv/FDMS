83af01e1863e1c29e332145791bfc6085827d05a
khanh <khanhcao0502@gmail.com>
2024-03-29 09:28:23 +0700

    Update alignment and text in HopDongTDG.php

--- app/Services/Document/DocumentExport/HopDongTDG.php
+++ app/Services/Document/DocumentExport/HopDongTDG.php
@@ -567,14 +567,14 @@ class HopDongTDG
 
         $row = $table->addRow();
         $row->addCell(700)->addText("a.", null, ['align' => 'right']);
-        $row->addCell(9300)->addText("Hai bên hoàn thành nghĩa vụ theo thỏa thuận trong hợp đồng;", null, ['align' => 'both']);
+        $row->addCell(9200)->addText("Hai bên hoàn thành nghĩa vụ theo thỏa thuận trong hợp đồng;", null, ['align' => 'both']);
         $row = $table->addRow();
         $row->addCell(700)->addText("b.", null, ['align' => 'right']);
         $row->addCell(9300)->addText("Hai bên thỏa thuận chấm dứt hợp đồng trước thời hạn:", null, ['align' => 'both']);
 
         $row = $table->addRow();
-        $row->addCell(800)->addText("6.3.", null, ['align' => 'right']);
-        $row->addCell(9300)->addText("Tuân thủ các quy định của pháp luật trong quá trình thực hiện thẩm định giá tài sản.", null, ['align' => 'both']);
+        $row->addCell(800)->addText("-", null, ['align' => 'right']);
+        $row->addCell(9100)->addText(" 	Trường hợp Bên A muốn chấm dứt hợp đồng trước hạn thì phải thông báo trước cho Bên B 01 ngày và phải thanh toán cho Bên B tiền phí dịch vụ tương ứng với khối lượng công việc Bên B đã thực hiện. Phí này do Bên B xác định và không thấp hơn phí đã thanh toán đợt 1.", null, ['align' => 'both']);
 
         $footer = $section->addFooter();
         $table = $footer->addTable();
