562ba229416368f9a8755108bcf9ab8cf9ecdd74
khanh <khanhcao0502@gmail.com>
2024-03-29 16:02:18 +0700

    Fix formatting in BienBanThanhLy.php

--- app/Services/Document/DocumentExport/BienBanThanhLy.php
+++ app/Services/Document/DocumentExport/BienBanThanhLy.php
@@ -229,31 +229,37 @@ class BienBanThanhLy
         $cellVTop = ['valign' => 'top'];
         $row1 = $table->addRow(100, array('tblHeader' => false, 'cantSplit' => false));
         $row1->addCell(1800, $cellVTop)->addText('BÊN A', ['bold' => true,],  $alignBoth);
-        $row1->addCell(8100, $cellVTop)->addText(': ' . $certificate->petitioner_name, ['bold' => true],  $alignBoth);
+        $row1->addCell(100, $cellVTop)->addText('', null,  $alignBoth);
+        $row1->addCell(8000, $cellVTop)->addText(': ' . $certificate->petitioner_name, ['bold' => true],  $alignBoth);
 
         $row2 = $table->addRow(100, array('tblHeader' => false, 'cantSplit'
         => false));
         $row2->addCell(1800, $cellVTop)->addText('-    Địa chỉ', null,  $alignBoth);
-        $row2->addCell(8100, $cellVTop)->addText(': ' . $certificate->petitioner_address, null,  $alignBoth);
+        $row2->addCell(100, $cellVTop)->addText('', null,  $alignBoth);
+        $row2->addCell(8000, $cellVTop)->addText(': ' . $certificate->petitioner_address, null,  $alignBoth);
 
         $row3 = $table->addRow(100, array('tblHeader' => false, 'cantSplit'
         => false));
         $row3->addCell(1800, $cellVTop)->addText('-    Mã số thuế', null,  $alignBoth);
-        $row3->addCell(8100, $cellVTop)->addText(': ', null,  $alignBoth);
+        $row3->addCell(100, $cellVTop)->addText('', null,  $alignBoth);
+        $row3->addCell(8000, $cellVTop)->addText(': ', null,  $alignBoth);
 
         $row4 = $table->addRow(100, array('tblHeader' => false, 'cantSplit'
         => false));
         $row4->addCell(1800, $cellVTop)->addText('-    Đại diện', null,  $alignBoth);
-        $row4->addCell(8100, $cellVTop)->addText(': ', null,  $alignBoth);
+        $row4->addCell(100, $cellVTop)->addText('', null,  $alignBoth);
+        $row4->addCell(8000, $cellVTop)->addText(': ', null,  $alignBoth);
 
-        $row5 = $table->addRow(100, array('tblHeader' => false, 'cantSplit' => false, 'spaceBefore' => 200));
+        $row5 = $table->addRow(100, array('tblHeader' => false, 'cantSplit' => false, 'spaceBefore' => 300));
         $row5->addCell(1800, $cellVTop)->addText('BÊN B', ['bold' => true,],  $alignBoth);
-        $row5->addCell(8100, $cellVTop)->addText(': CÔNG TY TNHH THẨM ĐỊNH GIÁ NOVA', ['bold' => true],  $alignBoth);
+        $row5->addCell(100, $cellVTop)->addText('', null,  $alignBoth);
+        $row5->addCell(8000, $cellVTop)->addText(': CÔNG TY TNHH THẨM ĐỊNH GIÁ NOVA', ['bold' => true],  $alignBoth);
 
         $row6 = $table->addRow(100, array('tblHeader' => false, 'cantSplit'
         => false));
         $row6->addCell(1800, $cellVTop)->addText('-    Địa chỉ', null,  $alignBoth);
-        $row6->addCell(8100, $cellVTop)->addText(': Số 728-730 Võ Văn Kiệt, Phường 1, Quận 5, TP.HCM', null,  $alignBoth);
+        $row6->addCell(100, $cellVTop)->addText('', null,  $alignBoth);
+        $row6->addCell(8000, $cellVTop)->addText(': Số 728-730 Võ Văn Kiệt, Phường 1, Quận 5, TP.HCM', null,  $alignBoth);
 
         $row7 = $table->addRow(100, array(
             'tblHeader' => false,
@@ -264,12 +270,17 @@ class BienBanThanhLy
 
         $row8 = $table->addRow(100, array('tblHeader' => false, 'cantSplit' => false));
         $row8->addCell(1800, $cellVTop)->addText('-   Mã số thuế', null,  $alignBoth);
-        $row8->addCell(8100, $cellVTop)->addText(': 0314514140', null,  $alignBoth);
+        $row8->addCell(100, $cellVTop)->addText('', null,  $alignBoth);
+        $row8->addCell(8000, $cellVTop)->addText(': 0314514140', null,  $alignBoth);
 
 
         $row9 = $table->addRow(100, array('tblHeader' => false, 'cantSplit' => false));
         $row9->addCell(1800, $cellVTop)->addText('-   Tài khoản số', null,  $alignBoth);
-        $row9->addCell(8100, $cellVTop)->addText(': 3101 00024 27729 tại Ngân hàng TMCP Đầu tư và Phát triển Việt Nam – CN Hồ Chí Minh – PGD Trần Hưng Đạo', null,  array('align' => 'both', 'textIndent' => 720));
+        $row9->addCell(
+            100,
+            $cellVTop
+        )->addText(':', null,  $alignBoth);
+        $row9->addCell(8000, $cellVTop)->addText(': 3101 00024 27729 tại Ngân hàng TMCP Đầu tư và Phát triển Việt Nam – CN Hồ Chí Minh – PGD Trần Hưng Đạo', null,  $alignBoth);
         $row9 = $table->addRow(100, array('tblHeader' => false, 'cantSplit' => false));
         $row9->addCell(1800, $cellVTop)->addText('-   Đại diện', null,  $alignBoth);
         $textRun = $row9->addCell(8100, $cellVTop)->addTextRun($alignBoth);
