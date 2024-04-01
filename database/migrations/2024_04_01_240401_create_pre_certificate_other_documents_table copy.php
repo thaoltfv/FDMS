124e968d2bf7324760fd0c72ed42140537cbff39
khanh <khanhcao0502@gmail.com>
2024-04-01 16:50:17 +0700

    migar

---
+++ database/migrations/2024_04_01_240401_create_pre_certificate_other_documents_table copy.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePreCertificateExportDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('pre_certificate_export_documents')) {
+            Schema::create('pre_certificate_export_documents', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('pre_certificate_id');
+                $table->foreign('pre_certificate_id')
+                    ->references('id')
+                    ->on('pre_certificates')
+                    ->onDelete('cascade');
+                $table->integer('certificate_id')->nullable();
+                $table->foreign('certificate_id')
+                    ->references('id')
+                    ->on('certificates')
+                    ->onDelete('cascade');
+                $table->text('name')->nullable();
+                $table->text('link')->nullable();
+                $table->text('type')->nullable();
+                $table->text('size')->nullable();
+                $table->text('description')->nullable();
+                $table->timestamp('created_at')->useCurrent();
+                $table->uuid('created_by')->nullable();
+                $table->foreign('created_by')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+                $table->timestamp('updated_at')->useCurrent();
+                $table->text('type_document');
+                $table->softDeletes();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pre_certificate_export_documents');
+    }
+}
