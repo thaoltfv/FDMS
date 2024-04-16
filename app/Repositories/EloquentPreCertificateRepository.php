34c5637151eb4be1a7c7ba50845fe4e72bd9e9ab
Hoang11012001 <lehuyhoang05022017@gmail.com>
2024-04-17 01:38:47 +0700

    query total

--- app/Repositories/EloquentPreCertificateRepository.php
+++ app/Repositories/EloquentPreCertificateRepository.php
@@ -625,6 +625,9 @@ class  EloquentPreCertificateRepository extends EloquentRepository implements Pr
         ];
         $with = [
             'createdBy:id,name',
+            'priceEstimates',
+            'priceEstimates.landFinalEstimate',
+            'priceEstimates.landFinalEstimate.total_price',
             'appraiserSale:id,name,user_id',
             'appraiserPerform:id,name,user_id',
             'appraiserBusinessManager:id,name,user_id',
