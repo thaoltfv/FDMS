5d71373494576441c4aebba63f990f0c932ed2b8
Hoang11012001 <lehuyhoang05022017@gmail.com>
2024-05-21 08:35:17 +0700

    edit bao cao

--- app/Services/Document/DocumentExport/KeHoachTDG.php
+++ app/Services/Document/DocumentExport/KeHoachTDG.php
@@ -222,7 +222,7 @@ class KeHoachTDG
 
         $row2 = $table->addRow();
         $row2->addCell(200)->addText(" -", null, ['align' => 'left']);
-        $row2->addCell(9700)->addText("Tài sản thẩm định giá: Giá trị" . $addressHSTD, null, $indentleftSymbol);
+        $row2->addCell(9700)->addText("Tài sản thẩm định giá: Giá trị " . $addressHSTD, null, $indentleftSymbol);
 
         $row3 = $table->addRow();
         $row3->addCell(200)->addText(" -", null, ['align' => 'left']);
