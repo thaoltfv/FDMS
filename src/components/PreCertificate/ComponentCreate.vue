fe60256205d3d3115e52652be938ea459e49f2cf
Hoang11012001 <lehuyhoang05022017@gmail.com>
2024-06-03 14:27:02 +0700

    fix giao dien

--- src/components/PreCertificate/ComponentCreate.vue
+++ src/components/PreCertificate/ComponentCreate.vue
@@ -141,8 +141,11 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-6">
-			<div class="card" :style="isMobile ? { 'margin-bottom': '70px' } : {}">
+		<div class="col-6 h-100">
+			<div
+				class="card"
+				:style="isMobile ? { 'margin-bottom': '70px' } : { height: '56vh' }"
+			>
 				<div class="card-title">
 					<div class="d-flex justify-content-between align-items-center">
 						<h3 class="title">Đối tác</h3>
