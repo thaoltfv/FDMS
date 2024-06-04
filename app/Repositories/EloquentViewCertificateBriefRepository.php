a2479f8ab08d5c6f396944ee34a82dbf658123e7
Hoang11012001 <lehuyhoang05022017@gmail.com>
2024-06-04 14:32:21 +0700

    check

--- app/Repositories/EloquentViewCertificateBriefRepository.php
+++ app/Repositories/EloquentViewCertificateBriefRepository.php
@@ -426,7 +426,7 @@ class EloquentViewCertificateBriefRepository extends EloquentRepository implemen
                 }
             });
         });
-        dd($result);
+        dd($result->get());
         $result =
             $result->select([
                 DB::raw("case status
