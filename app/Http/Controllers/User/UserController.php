4a2cf9ac0e8b163cecfa3765bce58f2d43081038
Hoang11012001 <lehuyhoang05022017@gmail.com>
2024-06-10 16:24:17 +0700

    str pass

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -403,7 +403,7 @@ class UserController extends Controller
     }
     public function randomPassword()
     {
-        $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!@#$%^&*()-_=+\|[]{};:/?.>";
+        $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!@#$";
         for ($i = 0; $i < 8; $i++) {
             $n = rand(0, strlen($alphabet) - 1);
             $pass[$i] = $alphabet[$n];
