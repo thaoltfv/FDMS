3603efed7935b43d4655a4649def90b320403ed3
Hoang11012001 <lehuyhoang05022017@gmail.com>
2024-06-10 16:30:45 +0700

    fix

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -403,11 +403,12 @@ class UserController extends Controller
     }
     public function randomPassword()
     {
-        $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+
+        $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!@#$";
         for ($i = 0; $i < 8; $i++) {
             $n = rand(0, strlen($alphabet) - 1);
             $pass[$i] = $alphabet[$n];
         }
-        return $pass;
+        return implode('', $pass);
     }
 }
