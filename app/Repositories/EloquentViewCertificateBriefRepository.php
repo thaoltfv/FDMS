7b7691ef45a3df82a3b4c3039bd746d14a7bfd03
Hoang11012001 <lehuyhoang05022017@gmail.com>
2024-06-14 17:40:22 +0700

    fix chart

--- app/Repositories/EloquentViewCertificateBriefRepository.php
+++ app/Repositories/EloquentViewCertificateBriefRepository.php
@@ -282,7 +282,7 @@ class EloquentViewCertificateBriefRepository extends EloquentRepository implemen
         $data = QueryBuilder::for(Certificate::class)
             ->select($select)
             ->whereIn('status', [1, 10, 2, 3, 7, 8, 9])
-            ->groupBy(['status', 'status_text'])
+            // ->groupBy(['status', 'status_text'])
             ->get()->toArray();
         $result = array('label' => Arr::pluck($data, 'status_text'), 'data' => Arr::pluck($data, 'count'), 'status' => Arr::pluck($data, 'status'));
         return $result;
