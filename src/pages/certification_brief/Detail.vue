c959ba226ae466bdb9cfd72af9669abb2cc5bd41
Hoang11012001 <lehuyhoang05022017@gmail.com>
2024-07-18 14:31:30 +0700

    check chuc vu

--- src/pages/certification_brief/Detail.vue
+++ src/pages/certification_brief/Detail.vue
@@ -3234,11 +3234,21 @@ export default {
 		if (
 			this.form.status &&
 			this.form.status === 9 &&
-			profile.data.user.roles[0].role_name.toUpperCase().includes("KẾ TOÁN")
+			this.convertToEnglish(profile.data.user.roles[0].role_name)
+				.toUpperCase()
+				.includes("KE TOAN")
 		) {
 			console.log("Vào if check hiện nút chỉnh sửa");
+			console.log(
+				"Convert vi to en",
+				this.convertToEnglish(profile.data.user.roles[0].role_name)
+			);
 			this.isAccounting = true;
 		} else {
+			console.log(
+				"Convert vi to en",
+				this.convertToEnglish(profile.data.user.roles[0].role_name)
+			);
 			console.log("Không vào if check hiện nút chỉnh sửa");
 			console.log("Role name", profile.data.user.roles[0].role_name);
 			console.log(
@@ -3463,6 +3473,119 @@ export default {
 		}
 	},
 	methods: {
+		convertToEnglish(str) {
+			const vietnameseChars = {
+				À: "A",
+				Á: "A",
+				Ả: "A",
+				Ã: "A",
+				Ạ: "A",
+				Ằ: "A",
+				Ắ: "A",
+				Ẳ: "A",
+				Ẵ: "A",
+				Ặ: "A",
+				È: "E",
+				É: "E",
+				Ẻ: "E",
+				Ẽ: "E",
+				Ẹ: "E",
+				Ề: "E",
+				Ế: "E",
+				Ể: "E",
+				Ễ: "E",
+				Ệ: "E",
+				Ì: "I",
+				Í: "I",
+				Ỉ: "I",
+				Ĩ: "I",
+				Ị: "I",
+				Ò: "O",
+				Ó: "O",
+				Ỏ: "O",
+				Õ: "O",
+				Ọ: "O",
+				Ồ: "O",
+				Ố: "O",
+				Ổ: "O",
+				Ỗ: "O",
+				Ộ: "O",
+				Ù: "U",
+				Ú: "U",
+				Ủ: "U",
+				Ũ: "U",
+				Ụ: "U",
+				Ừ: "U",
+				Ứ: "U",
+				Ử: "U",
+				Ữ: "U",
+				Ự: "U",
+				Ỳ: "Y",
+				Ý: "Y",
+				Ỷ: "Y",
+				Ỹ: "Y",
+				Ỵ: "Y",
+				Đ: "D",
+				à: "a",
+				á: "a",
+				ả: "a",
+				ã: "a",
+				ạ: "a",
+				ằ: "a",
+				ắ: "a",
+				ẳ: "a",
+				ẵ: "a",
+				ặ: "a",
+				è: "e",
+				é: "e",
+				ẻ: "e",
+				ẽ: "e",
+				ẹ: "e",
+				ề: "e",
+				ế: "e",
+				ể: "e",
+				ễ: "e",
+				ệ: "e",
+				ì: "i",
+				í: "i",
+				ỉ: "i",
+				ĩ: "i",
+				ị: "i",
+				ò: "o",
+				ó: "o",
+				ỏ: "o",
+				õ: "o",
+				ọ: "o",
+				ồ: "o",
+				ố: "o",
+				ổ: "o",
+				ỗ: "o",
+				ộ: "o",
+				ù: "u",
+				ú: "u",
+				ủ: "u",
+				ũ: "u",
+				ụ: "u",
+				ừ: "u",
+				ứ: "u",
+				ử: "u",
+				ữ: "u",
+				ự: "u",
+				ỳ: "y",
+				ý: "y",
+				ỷ: "y",
+				ỹ: "y",
+				ỵ: "y",
+				đ: "d"
+			};
+
+			return str.replace(
+				/[àáảãạăằắẳẵặèéẻẽẹêềếểễệìíỉĩịòóỏõọôồốổỗộùúủũụưừứửữựỳýỷỹỵđ]/g,
+				match => {
+					return vietnameseChars[match.toLowerCase()];
+				}
+			);
+		},
 		showPopupComfirmConvertDocumentView(type) {
 			this.typeConvert = type;
 			this.showPopupComfirmConvertDocument = true;
