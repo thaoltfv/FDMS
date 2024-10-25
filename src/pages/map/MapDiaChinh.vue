c6283be720b78b458ef28055c2f6f79f8c7e3962
Dat Ngo <datngo@192.168.1.3>
2024-10-25 08:48:36 +0700

    change map qh

--- src/pages/map/MapDiaChinh.vue
+++ src/pages/map/MapDiaChinh.vue
@@ -70,8 +70,9 @@ export default {
 	},
 	data() {
 		return {
-			url_modal:
-				"https://dev-web.fastvalue.com.vn/xem-quy-hoach/?mode=noHeader",
+			// url_modal:
+			// 	"https://dev-web.fastvalue.com.vn/xem-quy-hoach/?mode=noHeader",
+			url_modal: 'https://fastvalue.vn/xem-quy-hoach/?mode=noHeader',
 			seg_key: 0,
 			progress: 0,
 			emptyColorFill: {
