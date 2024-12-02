0fa52f40cda4cac8762fa91ebdf07c850ed6c43b
thaoltfv <thaolt@fastvalue.vn>
2024-12-02 14:17:16 +0700

    issue #29 update and optimize docker deployment on legacy_branches

--- backend/.ops/start.sh
+++ backend/.ops/start.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Starting up.."
+
+su - $USER_NAME -c "mkdir -p /srv/www/storage/api-docs"
+su - $USER_NAME -c "mkdir -p /srv/www/storage/app/public"
+su - $USER_NAME -c "mkdir -p /srv/www/storage/framework/cache/data"
+su - $USER_NAME -c "mkdir -p /srv/www/storage/framework/sessions"
+su - $USER_NAME -c "mkdir -p /srv/www/storage/framework/testing"
+su - $USER_NAME -c "mkdir -p /srv/www/storage/framework/views"
+su - $USER_NAME -c "mkdir -p /srv/www/storage/logs"
 
 supervisord -c /etc/supervisord.conf
